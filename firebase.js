0acf650b35a183588223b489b89f3198fb4471c0
touchmetender <theemperoryogiebear@gmail.com>
2025-04-29 13:18:09 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,20 @@
 <!-- firebase.js -->
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-  import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+<script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-database-compat.js"></script>
 
+<script>
   const firebaseConfig = {
     apiKey: "AIzaSyCXtiSnNBHs1NQXwL6vrK-U71veeMpIpXQ",
-  authDomain: "signagematerialdatabaseapp.firebaseapp.com",
-  databaseURL: "https://signagematerialdatabaseapp-default-rtdb.firebaseio.com",
-  projectId: "signagematerialdatabaseapp",
-  storageBucket: "signagematerialdatabaseapp.firebasestorage.app",
-  messagingSenderId: "276886100857",
-  appId: "1:276886100857:web:ec1b93f74995aa30e1ada8",
+    authDomain: "signagematerialdatabaseapp.firebaseapp.com",
+    databaseURL: "https://signagematerialdatabaseapp-default-rtdb.firebaseio.com",
+    projectId: "signagematerialdatabaseapp",
+    storageBucket: "signagematerialdatabaseapp.firebasestorage.app",
+    messagingSenderId: "276886100857",
+    appId: "1:276886100857:web:ec1b93f74995aa30e1ada8",
+    measurementId: "G-R4EHYT0MW6"
   };
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const db = getDatabase(app);
-
-  export { auth, db };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 </script>

4e754e4bafe9eb1087c402f2cec8834a3e54de20
touchmetender <theemperoryogiebear@gmail.com>
2025-04-29 11:11:47 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,13 @@
 // Replace the below with your Firebase project's config values
-const firebaseConfig = {
-  apiKey: "AIzaSyCXtiSnNBHs1NQXwL6vrK-U71veeMpIpXQ",
-  authDomain: "signagematerialdatabaseapp.firebaseapp.com",
-  databaseURL: "https://signagematerialdatabaseapp-default-rtdb.firebaseio.com",
-  projectId: "signagematerialdatabaseapp",
-  storageBucket: "signagematerialdatabaseapp.firebasestorage.app",
-  messagingSenderId: "276886100857",
-  appId: "1:276886100857:web:ec1b93f74995aa30e1ada8",
-  measurementId: "G-R4EHYT0MW6"
-};
-firebase.initializeApp(firebaseConfig);
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyCXtiSnNBHs1NQXwL6vrK-U71veeMpIpXQ",
+    authDomain: "signagematerialdatabaseapp.firebaseapp.com",
+    databaseURL: "https://signagematerialdatabaseapp-default-rtdb.firebaseio.com",
+    projectId: "signagematerialdatabaseapp",
+    storageBucket: "signagematerialdatabaseapp.firebasestorage.app",
+    messagingSenderId: "276886100857",
+    appId: "1:276886100857:web:ec1b93f74995aa30e1ada8"
+  };
+  firebase.initializeApp(firebaseConfig);
+</script>

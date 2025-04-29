3189baed8d0ac397ad3376561414f5a2e86d71be
touchmetender <theemperoryogiebear@gmail.com>
2025-04-29 13:13:09 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,22 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCXtiSnNBHs1NQXwL6vrK-U71veeMpIpXQ",
+<!-- firebase.js -->
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+  import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCXtiSnNBHs1NQXwL6vrK-U71veeMpIpXQ",
   authDomain: "signagematerialdatabaseapp.firebaseapp.com",
   databaseURL: "https://signagematerialdatabaseapp-default-rtdb.firebaseio.com",
   projectId: "signagematerialdatabaseapp",
   storageBucket: "signagematerialdatabaseapp.firebasestorage.app",
   messagingSenderId: "276886100857",
   appId: "1:276886100857:web:ec1b93f74995aa30e1ada8",
-  measurementId: "G-R4EHYT0MW6"
-};
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getDatabase(app);
+
+  export { auth, db };
+</script>

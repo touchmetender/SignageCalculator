ea8135bca44b6b0a3295111ff4a1218a54ca0f69
touchmetender <theemperoryogiebear@gmail.com>
2025-04-29 11:16:09 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,21 @@
-// Replace the below with your Firebase project's config values
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyCXtiSnNBHs1NQXwL6vrK-U71veeMpIpXQ",
-    authDomain: "signagematerialdatabaseapp.firebaseapp.com",
-    databaseURL: "https://signagematerialdatabaseapp-default-rtdb.firebaseio.com",
-    projectId: "signagematerialdatabaseapp",
-    storageBucket: "signagematerialdatabaseapp.firebasestorage.app",
-    messagingSenderId: "276886100857",
-    appId: "1:276886100857:web:ec1b93f74995aa30e1ada8"
-  };
-  firebase.initializeApp(firebaseConfig);
-</script>
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXtiSnNBHs1NQXwL6vrK-U71veeMpIpXQ",
+  authDomain: "signagematerialdatabaseapp.firebaseapp.com",
+  databaseURL: "https://signagematerialdatabaseapp-default-rtdb.firebaseio.com",
+  projectId: "signagematerialdatabaseapp",
+  storageBucket: "signagematerialdatabaseapp.firebasestorage.app",
+  messagingSenderId: "276886100857",
+  appId: "1:276886100857:web:ec1b93f74995aa30e1ada8"
+};
+
+const app = initializeApp(firebaseConfig);
+const firebase = {
+  auth: getAuth(app),
+  database: getDatabase(app)
+};
+window.firebase = firebase;

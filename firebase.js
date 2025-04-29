b7fc093caff2cb4cd940cd2b7f260857acf7e58b
touchmetender <theemperoryogiebear@gmail.com>
2025-04-29 13:08:47 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,25 +1,11 @@
-// firebase.js
 const firebaseConfig = {
-  apiKey: "AIzaSyDZHl03Lrk0onP4ZAfzK2gCoAvRljkVfIw",
+  apiKey: "AIzaSyD7-xJxuJj0fVdqQG1_A0G3jYrON5vHrlQ",
   authDomain: "signagematerialdatabaseapp.firebaseapp.com",
   databaseURL: "https://signagematerialdatabaseapp-default-rtdb.firebaseio.com",
   projectId: "signagematerialdatabaseapp",
   storageBucket: "signagematerialdatabaseapp.appspot.com",
-  messagingSenderId: "984103840861",
-  appId: "1:984103840861:web:4c26e11c92f64acb65d2f8"
+  messagingSenderId: "471449176777",
+  appId: "1:471449176777:web:7f576be671bcadfa68d7ab"
 };
 
 firebase.initializeApp(firebaseConfig);
-
-function logout() {
-  firebase.auth().signOut().then(() => {
-    window.location.href = "login.html";
-  });
-}
-
-firebase.auth().onAuthStateChanged(user => {
-  const userInfo = document.getElementById('user-info');
-  if (user && userInfo) {
-    userInfo.textContent = `Logged in as: ${user.email}`;
-  }
-});
